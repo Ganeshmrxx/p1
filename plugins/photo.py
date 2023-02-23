c944819f70e1e8f1c823399d93c4b2bb77db9355
Ganeshmrxx <100681292+Ganeshmrxx@users.noreply.github.com>
2023-02-23 11:49:32 +0530

    Add files via upload

---
+++ plugins/photo.py
@@ -0,0 +1,17 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.photo & filters.private)
+async def photo(client: Client, message: Message):
+    try:
+        ok ='ok'
+    except Exception as e:
+        print("photomarkup error - " + str(e))
+        if "USER_IS_BLOCKED" in str(e):
+            return
+        else:
+            try:
+                await message.reply_text(f"{e} \nSomething went wrong!", quote=True)
+            except Exception:
+                return
